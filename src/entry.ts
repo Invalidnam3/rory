bcb50312a784cc6ef3101334229d86c990eefe09
Andrés <andres.acostahe@gmail.com>
2022-10-10 18:34:49 -0500

    Add audioPlayer and first resource

--- src/entry.ts
+++ src/entry.ts
@@ -1,14 +1,34 @@
 import { config } from 'dotenv'
 import { Client, GatewayIntentBits } from 'discord.js'
-import { joinVoiceChannel, DiscordGatewayAdapterCreator } from '@discordjs/voice'
+import {
+  joinVoiceChannel,
+  createAudioPlayer,
+  createAudioResource,
+  VoiceConnection
+} from '@discordjs/voice'
+import { join } from 'node:path'
+import { exec } from 'child_process'
 
 // load .env file into process.env
 config()
 
+// Test for downloading an mp3 file from spotify
+exec('spotifydl https://open.spotify.com/track/0x7vEcjqMVjuKvvsIUGEPT?si=5738563cb4e74c12', (err, stdout, stderr) => {
+  if (err) {
+    // node couldn't execute the command
+    console.log(err)
+    return;
+  }
+
+  // the *entire* stdout and stderr (buffered)
+  console.log(`stdout: ${stdout}`)
+  console.log(`stderr: ${stderr}`)
+})
+
 const client = new Client({
   // Define bot intectactions scope
   intents: [
-    GatewayIntentBits.Guilds, 
+    GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.GuildVoiceStates,
     GatewayIntentBits.MessageContent,
@@ -24,14 +44,28 @@ client.on('messageCreate', (message) => {
   // Check if member is part of the current guild
   if (message.member) {
     const { channel } = message.member.voice
+    const audioPlayer = createAudioPlayer()
+    // audioPlayer.on('error', error => {
+    //   console.log(error)
+    // })
+    // audioPlayer.on('stateChange', stateChange => {
+    //   console.log(stateChange.status)
+    // })
+    let voiceConnection: VoiceConnection
     if (channel) {
-      joinVoiceChannel({
+      voiceConnection = joinVoiceChannel({
         channelId: channel.id,
         guildId: channel.guild.id,
         adapterCreator: channel.guild.voiceAdapterCreator
       })
+      // Sample resource afterdark
+      console.log(join(__dirname, 'afterdark.mp3'))
+      const resource = createAudioResource(join(__dirname, 'afterdark.mp3'))
+      // console.log(resource)
+      audioPlayer.play(resource)
+      voiceConnection.subscribe(audioPlayer)
     }
   }
 })
 
-client.login(process.env.TOKEN)
+// client.login(process.env.TOKEN)
